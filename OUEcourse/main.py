beb1504fbc274d81d78624f5f27245bcc9bd82cf
nguyennhat2401 <nhatnt2401@gmail.com>
2025-08-20 15:28:08 +0700

    UI update

--- OUEcourse/main.py
+++ OUEcourse/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from flask import render_template
+from __init__ import app
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
